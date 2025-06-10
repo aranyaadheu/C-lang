1617fa10fe99eb49beff94779645bf32f98c31d4
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-10 23:33:11 +0600

    input using scanf

---
+++ first programs/input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int x;
+
+    printf("Enter a Number: ");
+    scanf("%d", &x); // takes a user input
+    printf("You entered %d", x);
+    return 0;
+}
