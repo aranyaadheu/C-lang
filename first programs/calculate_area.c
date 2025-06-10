b151abb92d0b1a1ec5e5ce4ce8c3701383b8723e
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-10 23:27:27 +0600

    Create calculate_area.c

---
+++ first programs/calculate_area.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int length = 4;
+    int width = 6;
+    int area;
+
+    // calculate the area of a rectangle
+    area = length * width;
+
+    printf("Length is %d\n", length);
+    printf("Width is %d\n", width);
+    printf("Area of the recatangle is %d\n", area);
+}
