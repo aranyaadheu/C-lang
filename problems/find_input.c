16eb8abd04bbcf20a56774b1683ea620cb50e218
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-19 11:15:38 +0600

    input using ASCII values

--- problems/find_input.c
+++ problems/find_input.c
@@ -3,10 +3,26 @@
 #include <stdio.h>
 
 int main(void) {
+// solve this using ASCII value
+/*
+ASCII values:
+A-Z ----> 65 to 90
+a-z ----> 97 to 122
+0-9 ----> 48 to 57
+*/
     char ch;
     printf("Enter your character: ");
     scanf("%c", &ch);
+
+    if((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)){
+        printf("%c is an Alphabet.\n", ch);
+    } else if(ch >= 48 && ch <= 67){
+        printf("%c is a Digit.\n", ch);
+    } else{
+        printf("%c is a Special Character.\n", ch);
+    }
     
+    /*  
     if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
         printf("%c is an Alphabet.\n", ch);
     } else if (ch >= '0' && ch <= '9'){
@@ -14,6 +30,6 @@ int main(void) {
     } else {
         printf("%c is a Special Character.\n", ch);
     }
-
+*/
     return 0;
 }
