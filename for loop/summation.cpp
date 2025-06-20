f0e472fa4773983b11b2dc74600e32f4c53b04ce
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-21 01:01:53 +0600

    n(n+1)/2

---
+++ for loop/summation.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    sum = n * (n+1) / 2;
+    printf("Sum of first %d natural number is: %d\n", n, sum);
+
+    return 0;
+}
