34812f6c3c7d9a11b130bdee95c2638675b71a89
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-26 15:29:41 +0600

    while loop ex

---
+++ while loop/prime.c
@@ -0,0 +1,13 @@
+// prime or not using while loop
+#include <stdio.h>
+
+int main(){
+    int i, n;
+    printf("Enter a number: ");
+    scanf("%d\n", &n);
+    while(i <= n){
+        if ()
+    }
+
+    return 0;
+}
