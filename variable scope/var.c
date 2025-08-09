8003b0dfdb884f5207f621ee5fd3ae66cac163c1
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-08-09 11:40:33 +0600

    Create var.c

---
+++ variable scope/var.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int add(int x, int y){
+    int result = x + y;
+    return result;
+}
+
+int subtract(int x, int y){
+    int result = x - y;
+    return result;
+}
+
+int main(){
+    // variable scope = Refers to where a variable is recognize and accessible.
+    //                  Variables can share the same name if
+    //                  they're in different scopes{}
+
+    int x = 5;
+    int y = 6;
+    // int result = subtract(3, 4);
+    int result = add(x, y);
+
+    printf("%d", result);
+
+    return 0;
+}
