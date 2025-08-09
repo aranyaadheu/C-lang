fea4811f925c249a88cded2f4a028208c83a7341
Aniruddha Aranya A Dheu <125354418+aranyaadheu@users.noreply.github.com>
2025-08-09 21:33:19 +0600

    fix: syntax error.

--- problems/vowel_consonant.c
+++ problems/vowel_consonant.c
@@ -5,7 +5,7 @@ int main(void){
     printf("Enter a Character: ");
     scanf("%c", &ch);
     
-    if((ch >='"A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+    if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
         if(ch == 'A' || ch== 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
             printf("%c is a Vowel!!!!\n", ch);
         }
