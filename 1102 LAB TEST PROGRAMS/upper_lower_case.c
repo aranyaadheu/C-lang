41384b8d439f4365d436f96cc00101a46e152a7a
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-09-05 19:06:34 +0600

    Create upper_lower_case.c

---
+++ 1102 LAB TEST PROGRAMS/upper_lower_case.c
@@ -0,0 +1,18 @@
+// upper case or lower case
+#include <stdio.h>
+
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if(ch >= 'A' && ch <= 'Z'){
+        printf("Upper case\n");
+    } else if (ch >= 'a' && ch <= 'z'){
+        printf("Lower case\n");
+    } else{
+        printf("Invalid character!\n");
+    }
+
+    return 0;
+}
